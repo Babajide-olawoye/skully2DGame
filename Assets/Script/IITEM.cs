27a84b43bf2213e50e271766c400a1a21412703b
Babajide Joshua Oke <48318533+Babajide-olawoye@users.noreply.github.com>
2025-07-14 23:40:30 +0100

    Update IITEM.cs

--- Assets/Script/IITEM.cs
+++ Assets/Script/IITEM.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
+// This is an item interface. Give to obj that wants
+// to be define as an item
 public interface IITEM
 {
+    // Method to be called when the item is collected
     public void Collect();
 }
