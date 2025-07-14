881fe713754f92291928c1f9d8c1c0024f1026fa
Babajide Joshua Oke <48318533+Babajide-olawoye@users.noreply.github.com>
2025-07-14 23:38:34 +0100

    Update Gem.cs

--- Assets/Script/Gem.cs
+++ Assets/Script/Gem.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
+// This class represents a Gem obj.
+// It implements the IITEM interface, so it can be collected.
 public class Gem : MonoBehaviour, IITEM
 {
+    // Destroys the GameObject.
     public void Collect()
     {
         Destroy(gameObject);
